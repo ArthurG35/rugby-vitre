9893fbf327e019c7359bd8b557266ca3f54b5df5
ArthurG <arthur.gougeon@epsi.fr>
2022-09-01 22:30:03 +0200

    Config Save

--- src/app/equipe/equipe.module.ts
+++ src/app/equipe/equipe.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from '@angular/common';
 import {EquipeRoutingModule} from './equipe-routing.module';
 import {PageListEquipeComponent} from './page/page-list-equipe/page-list-equipe.component';
 import {CoreModule} from "../core/core.module";
-import {BrowserModule} from "@angular/platform-browser";
 
 
 @NgModule({
--- src/app/equipe/services/joueurs.service.spec.ts
+++ src/app/equipe/services/joueurs.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { JoueursService } from './joueurs.service';
+import {JoueursService} from './joueurs.service';
 
 describe('JoueursService', () => {
   let service: JoueursService;
