8cc2c6279d3f2a1245ec8eb8ee3e283b4cc9a36d
ArthurG35 <arthur.gougeon@epsi.com>
2022-09-25 22:28:38 +0200

    Angular.json Add Proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {environment} from "../environments/environment";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'rugby-vitre';
+
+  constructor() {
+    console.log(environment.production); // Logs false for default environment
+  }
 }
